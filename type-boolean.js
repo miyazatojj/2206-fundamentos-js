0ad2a0e443b94ed2713e5592fbf3514072a0882b
miyazatojj <miyazatojj@gmail.com>
2021-09-09 16:34:20 -0300

    exemplos da aula 1

---
+++ type-boolean.js
@@ -0,0 +1,17 @@
+// comparações sempre retornam True ou False
+// === -> usado para comparar
+// exemplo: para comparar se a senha inserida é a mesma que está guardada no banco de dados
+// exemplo: se o cadastro está ativado, então é True
+// booleanos podem assumir valores de variáveis
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+
+// console.log(primeiroNumero === segundoNumero);
+
+let cadastroAtivado = true;
+
+// comparando strings
+const texto1 = "Alura";
+const texto2 = "alura";
+console.log(texto1 === texto2);
